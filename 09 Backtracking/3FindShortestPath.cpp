cf81dbaf36c96e0bc03cefdb67f5fe185f80093b
Arpit <arpitfalcon1@gmail.com>
2021-05-29 11:34:49 +0530

    backtracking: find shortest path

---
+++ 09 Backtracking/3FindShortestPath.cpp
@@ -0,0 +1,69 @@
+bool isValid(int i, int j, vector<vector<int>> grid) {
+	if (i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size())
+		return false;
+
+	return true;
+}
+
+
+void markUnsafeTiles(vector<vector<int>> &grid) {
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			// If we have a landmine here, we will mark the neighboring tiles as unsafe
+			if (grid[i][j] == 0) {
+				// Up
+				if (isValid(i - 1, j, grid))
+					grid[i - 1][j] = -1;
+				// Down
+				if (isValid(i + 1, j, grid))
+					grid[i + 1][j] = -1;
+				// Left
+				if (isValid(i, j - 1, grid))
+					grid[i][j - 1] = -1;
+				// Right
+				if (isValid(i, j + 1, grid))
+					grid[i][j + 1] = -1;
+			}
+		}
+	}
+
+	// Now mark all the unsafe areas
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			if (grid[i][j] == -1)
+				grid[i][j] = 0;
+		}
+	}
+
+	return;
+}
+
+
+void findShortestPath(vector<vector<int>> &grid, int i, int j, int currDistance, int &minDistance) {
+	// Base Case
+	if (j == grid[0].size()) {
+		minDistance = min(minDistance, currDistance);
+		return;
+	}
+
+	// Is we encounter the unsafe position
+	if (grid[i][j] == 0)
+		return;
+
+	// Pruning
+	if (currDistance > minDistance)
+		return;
+
+	// Recursive Case
+	// Mark as visited and then visit in all four directions
+	grid[i][j] = 0;
+	// Down, Left, Right, Up
+	findShortestPath(grid, i + 1, j, currDistance + 1, minDistance);
+	findShortestPath(grid, i, j - 1, currDistance + 1, minDistance);
+	findShortestPath(grid, i, j + 1, currDistance + 1, minDistance);
+	findShortestPath(grid, i - 1, j, currDistance + 1, minDistance);
+
+	// Backtrack
+	grid[i][j] = 1;
+	return;
+}
--- run.cpp
+++ run.cpp
@@ -35,44 +35,99 @@ const double PI = 3.141592653589793238463;
 /**** Your code goes here - ****/
 /*******************************/
 
-bool isPossibleEqualPartition(int arr[], int n, int i, int currSum, int totalSum) {
-	// Base Cases
-	// We can divide the array into 2 parts
-	if (currSum == totalSum / 2)
-		return true;
 
-	// We can't divide with this configuration
-	if (currSum > totalSum / 2)
+bool isSafe() {
+
+}
+
+bool isValid(int i, int j, vector<vector<int>> grid) {
+	if (i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size())
 		return false;
 
-	// Array is exhausted
-	if (i == n)
-		return false;
-
-	// Recursive Cases
-	// Include the current element
-	bool includeCase = isPossibleEqualPartition(arr, n, i + 1, currSum + arr[i], totalSum);
-
-	// Exclude the current element
-	bool excludeCase = isPossibleEqualPartition(arr, n, i + 1, currSum, totalSum);
-
-	// Is this case a valid config?
-	return includeCase or excludeCase;
+	return true;
 }
 
 
-int equalPartition(int N, int arr[]) {
-	int sum = 0;
-	for (int i = 0; i < N; i++) {
-		sum += arr[i];
+void markUnsafeTiles(vector<vector<int>> &grid) {
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			// If we have a landmine here, we will mark the neighboring tiles as unsafe
+			if (grid[i][j] == 0) {
+				// Up
+				if (isValid(i - 1, j, grid))
+					grid[i - 1][j] = -1;
+				// Down
+				if (isValid(i + 1, j, grid))
+					grid[i + 1][j] = -1;
+				// Left
+				if (isValid(i, j - 1, grid))
+					grid[i][j - 1] = -1;
+				// Right
+				if (isValid(i, j + 1, grid))
+					grid[i][j + 1] = -1;
+			}
+		}
 	}
 
-	// If sum is odd
-	if (sum & 1)
-		return false;
+	// Now mark all the unsafe areas
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			if (grid[i][j] == -1)
+				grid[i][j] = 0;
+		}
+	}
 
+	return;
+}
+
+
+void findShortestPath(vector<vector<int>> &grid, int i, int j, int currDistance, int &minDistance) {
+	// Base Case
+	if (j == grid[0].size()) {
+		minDistance = min(minDistance, currDistance);
+		return;
+	}
+
+	// Is we encounter the unsafe position
+	if (grid[i][j] == 0)
+		return;
+
+	// Pruning
+	if (currDistance > minDistance)
+		return;
+
+	// Recursive Case
+	// Mark as visited and then visit in all four directions
+	grid[i][j] = 0;
+	// Down, Left, Right, Up
+	findShortestPath(grid, i + 1, j, currDistance + 1, minDistance);
+	findShortestPath(grid, i, j - 1, currDistance + 1, minDistance);
+	findShortestPath(grid, i, j + 1, currDistance + 1, minDistance);
+	findShortestPath(grid, i - 1, j, currDistance + 1, minDistance);
+
+	// Backtrack
+	grid[i][j] = 1;
+	return;
+}
+
+
+vector<vector<int> > combinationSum(vector<int> &A, int B) {
+	vector<vector<int>> res;
 	int currSum = 0;
-	return isPossibleEqualPartition(arr, N, 0, currSum, sum);
+	vector<int> currVec;
+
+	getAllCombinatinSums(A, B, currSum, currVec, res, 0);
+
+	// Do the computations
+	set<vector<int>> result(res.begin(), res.end());
+	vector<vector<int>> finalRes(result.begin(), result.end());
+
+	for (int i = 0; i < finalRes.size(); i++) {
+		sort(finalRes[i].begin(), finalRes[i].end());
+	}
+
+	sort(finalRes.begin(), finalRes.end());
+	return finalRes;
 }
 
 
@@ -86,11 +141,18 @@ int main() {
 
 	int n;
 	cin >> n;
-	int arr[n];
+	vector<int> arr(n);
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
+	int a; cin >> a;
 
-	cout << equalPartition(n, arr);
+	vector<vector<int>> res = combinationSum(arr, a);
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++) {
+			cout << res[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
 
