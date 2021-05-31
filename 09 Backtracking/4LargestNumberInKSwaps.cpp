81eda604a9178d11509a20c19faf162a7b4d79a3
Arpit <arpitfalcon1@gmail.com>
2021-05-31 16:09:56 +0530

    backtracking: larget number in k swaps efficient

--- 09 Backtracking/4LargestNumberInKSwaps.cpp
+++ 09 Backtracking/4LargestNumberInKSwaps.cpp
@@ -1,4 +1,5 @@
-// TLE Solution - (N!)
+// Slow Solution - O(N^2 ^ K)
+// Space - O(N)
 void findMaximumBacktracking(string &str, int k, int idx, int swaps, string &maxNum) {
 	// Base Case
 	if (swaps == k or idx == str.size()) {
@@ -10,15 +11,20 @@ void findMaximumBacktracking(string &str, int k, int idx, int swaps, string &max
 
 	// Recursive Case
 	for (int i = idx; i < str.size(); i++) {
-		swap(str[i], str[idx]);
-		if (i == idx)
-			findMaximumBacktracking(str, k, idx + 1, swaps, maxNum);
-		else
-			findMaximumBacktracking(str, k, idx + 1, swaps + 1, maxNum);
+		if (str[i] >= str[idx]) {
+			swap(str[i], str[idx]);
+			if (i == idx)
+				findMaximumBacktracking(str, k, idx + 1, swaps, maxNum);
+			else
+				findMaximumBacktracking(str, k, idx + 1, swaps + 1, maxNum);
 
-		// Backtrack
-		swap(str[i], str[idx]);
+			// Backtrack
+			swap(str[i], str[idx]);
+		}
 	}
 
 	return;
 }
+
+
+// Optimization - search for the max string from the left ones and swap that only.
