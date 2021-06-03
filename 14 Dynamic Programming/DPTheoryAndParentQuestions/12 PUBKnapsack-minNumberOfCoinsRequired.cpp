30069c55357a58f111e938f580cbc69124949e73
Arpit <arpitfalcon1@gmail.com>
2021-06-03 10:44:04 +0530

    ubk: min coins, 1D dp

--- 14 Dynamic Programming/DPTheoryAndParentQuestions/12 PUBKnapsack-minNumberOfCoinsRequired.cpp
+++ 14 Dynamic Programming/DPTheoryAndParentQuestions/12 PUBKnapsack-minNumberOfCoinsRequired.cpp
@@ -17,6 +17,7 @@ int minCoinsToCoinChange(int coins[], int n, int coinValue) {
 	memset(dp, 0, sizeof(dp));
 
 	// Empty subset pe toh koi coin value ni milegi, so we take inf coins and make it.
+	// Also, INT_MAX - 1 to avoid overflow when we do +1 while building up.
 	for (int j = 0; j <= coinValue; j++)
 		dp[0][j] = INT_MAX - 1;
 	// Kyunki empty subsets honge when coinValue is 0, therefore 0 coins.
@@ -44,4 +45,26 @@ int minCoinsToCoinChange(int coins[], int n, int coinValue) {
 	}
 
 	return dp[n][coinValue];
+}
+
+
+// 2nd Approach - using 1D vector
+int coinChange(vector<int>& coins, int amount) {
+	// Initialization
+	vector<int> dp(amount + 1, 0);
+	for (int i = 0; i <= amount; i++)
+		dp[i] = INT_MAX - 1;
+	dp[0] = 0;
+
+	// Build up the dp
+	for (int i = 0; i <= amount; i++) {
+		for (int j = 0; j < coins.size(); j++) {
+			if (i - coins[j] >= 0) {
+				//           Include             Exclude
+				dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
+			}
+		}
+	}
+
+	return dp[amount] == INT_MAX - 1 ? -1 : dp[amount];
 }
