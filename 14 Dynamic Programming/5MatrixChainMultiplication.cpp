fb4ee00eede07658986208aaf901a1bc4050c3e7
Arpit <arpitfalcon1@gmail.com>
2021-06-06 12:08:52 +0530

    dp: mcm

---
+++ 14 Dynamic Programming/5MatrixChainMultiplication.cpp
@@ -0,0 +1,21 @@
+int mcmTopDown(int arr[], int i, int j, int dp[][]) {
+	// Base Case
+	if (i >= j)
+		return dp[i][j] = 0;
+
+	// Look up the table
+	if (dp[i][j] != -1)
+		return dp[i][j];
+
+	int ans = INT_MAX;
+	// Recursive Case
+	for (int k = i; k < j; k++) {
+		int tempAns = mcmTopDown(arr, i, k, dp)
+		              + mcmTopDown(arr, k + 1, j, dp)
+		              + arr[i - 1] * arr[k] * arr[j];
+
+		int ans = min(tempAns, ans);
+	}
+
+	return ans;
+}
