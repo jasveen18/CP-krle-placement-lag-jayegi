e90c0c690761a35f1e8c8efcced0c662b6a4efcc
Arpit <arpitfalcon1@gmail.com>
2021-06-12 09:59:13 +0530

    string: rabin karp algo

--- CompetitiveProg_CB/String Matching Algorithms/rabin-karp-algo.cpp
+++ CompetitiveProg_CB/String Matching Algorithms/rabin-karp-algo.cpp
@@ -1,38 +1,3 @@
-/******************************************
-* AUTHOR : ARPIT *
-* NICK : arpitfalcon *
-* INSTITUTION : BIT MESRA *
-******************************************/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-#define endl "\n"
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define max3(a, b, c) max(a, max(b, c))
-#define min3(a, b, c) min(a, min(b, c))
-#define max4(a, b, c, d) max(a, max3(b, c, d))
-#define min4(a, b, c, d) min(a, min3(b, c, d))
-
-#define TestCase ll t = 1; cin >> t; while (t--)
-#define vin ll n; cin >> n; vector<int> v(n); for (ll i = 0; i < n; i++) cin >> v[i]
-#define vout for (ll i = 0; i < n; i++) cout << v[i] << " "
-#define ain ll n, k; cin >> n >> k; ll a[n]; for (ll z = 0; z < n; z++) cin >> a[z]
-#define aout for (ll i = 0; i < n; i++) cout << a[i] << " "
-
-#define blink                \
-    ios::sync_with_stdio(0); \
-    cin.tie(0);              \
-    cout.tie(0);
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-const double PI = 3.141592653589793238463;
-
-/********************************/
-/**** Your code goes here - ****/
-/*******************************/
 // Rabin Karp is used to match one string to another.
 // First calculate the hash of the pattern, now we need to find which
 // window of same length of the string matches with the hash of the pattern.
@@ -133,17 +98,4 @@ void rabinKarpAlgo(string text, string pattern) {
 		// Modify to move on to next window
 		textHash = hashNext;
 	}
-}
-
-
-int main() {
-	blink
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-
-	rabinKarpAlgo("abbabs", "ab");
-
-	return 0;
 }
