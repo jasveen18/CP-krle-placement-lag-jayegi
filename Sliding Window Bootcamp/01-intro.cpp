5daeb7dc373e5c36f3c26ed30f0cec9c9a9941e2
Arpit <arpitfalcon1@gmail.com>
2021-06-16 10:03:11 +0530

    slinding-window:intro

---
+++ Sliding Window Bootcamp/01-intro.cpp
@@ -0,0 +1,26 @@
+// *** SLIDING WINDOW *** //
+
+// Given an array, uska subarrays banega (continous wali chiz).
+// Subarray given size ka, uspe kaam krna hoga.
+
+// Ek window bana re hai, phir movement aise hoga ki ek element add krta hu and last wali ko hata deta hu.
+
+// How to identify sliding window? ->
+// Array / String ka question hoga
+// Subarray / substring puchega
+// k dia hoga, window size
+// Min / Max pucha hoga
+// Not hard thing, sochna hoga aisa dia hoga.
+
+// Kabhi kabhi window size ni degi, voi nikalna hoga.
+
+// Types of Sliding window -
+// 1. Fixed Size Window. Easyy
+// Ek k size ka window bana kr hmlog move krte rhenge.
+
+// 2. Variable Size Window. Relatively toughh
+// Isme window size dia ni hota hai, isko change kr skte hai and voi puch hota hai.
+// Largest / Smallest window puchi hoti hai subjected to a question.
+
+
+
