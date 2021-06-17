6348fa74f7588891f5785f079bd220c5de9f5911
Arpit <arpitfalcon1@gmail.com>
2021-06-17 10:28:50 +0530

    count occurances of anagram

---
+++ Sliding Window Bootcamp/04-countOccuranceOfAnagram.cpp
@@ -0,0 +1,53 @@
+// *** Count Occurance of Anagram *** //
+
+// Problem Statement -
+// Given two strings, 2nd string ki saari anagrams ko find krni hai in 1st string.
+// Uska count dena hai.
+
+// Problem Identification -
+// 1. String di hui hai, ek pattern dia hua hai.
+// 2. Substring nikalna hai, anagram form krra hai ya ni.
+// 3. Window size bhi dekho dia hai, that is length of 2nd string(pattern).
+
+int countOccurenceAnagrams(string text, string pattern) {
+	int size = text.size();
+	int k = pattern.size();
+	int countOccurances = 0;
+	int countCharPresent = 0;
+	unordered_map<int, int> charCount;
+
+	for (int i = 0; i < k; i++)
+		charCount[pattern[i]]++;
+	// Update the size of map.
+	countCharPresent = charCount.size();
+
+	int i = 0, j = 0;
+	while (j < size) {
+		// Operation for new element
+		if (charCount.find(text[j]) != charCount.end()) {
+			charCount[text[j]]--;
+
+			// Agar zero ho jaara hai toh unique char count ko decrement kr do
+			if (charCount[text[j]] == 0)
+				countCharPresent--;
+		}
+
+		// If window size ni bani ab tk
+		if (j - i + 1 < k) {
+			j++;
+		} else if (j - i + 1 == k) {
+			// Ab check krlo ki number of elements match krra hai ya ni
+			// Agar count pe kuch hai hi ni toh mtlb match krra hai
+			if (countCharPresent == 0)
+				countOccurances++;
+
+			// Ab slide krenge
+			if (charCount.find(text[i]) != charCount.end()) {
+				charCount[text[i]]++;
+			}
+			i++; j++;
+		}
+	}
+
+	return countOccurances;
+}
