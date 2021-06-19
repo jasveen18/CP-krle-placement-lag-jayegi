65a5fcd1136d1a2ddd016575903053a50c516bef
Arpit <arpitfalcon1@gmail.com>
2021-06-19 20:51:31 +0530

    search: count squares

--- 450 DSA Questions/04 Searching & Sorting/3CountSquares.cpp
+++ 450 DSA Questions/04 Searching & Sorting/3CountSquares.cpp
@@ -9,17 +9,17 @@
 // So basically, floor of sqrt chaiye.
 
 int countSquares(int N) {
-    start = 0, end = N - 1;
+    long long int start = 0, end = N - 1;
 
     while (start <= end) {
-        int mid = (start + end) / 2;
-        if (mid * mid == N - 1)
-            return mid;
-        else if (mid * mid > N - 1)
+        long long int mid = ((end - start) / 2) + start;
+
+        if (mid * mid >= N)
             end = mid - 1;
-        else
+        else {
             start = mid + 1;
+        }
     }
 
-    return start;
+    return end;
 }
