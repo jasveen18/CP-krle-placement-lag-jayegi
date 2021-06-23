3b2759f5c2c1b71a1eacab83ec6ee43d2e97ed32
Arpit <arpitfalcon1@gmail.com>
2021-06-23 21:47:09 +0530

    ll: find the starting point of loop

---
+++ 450 DSA Questions/05 LinkedList/3StartingPointOfLoop.cpp
@@ -0,0 +1,36 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Return the starting point of Loop in a LL.
+
+void removeLoopLL(Node* head) {
+	Node* fast = head, *slow = head;
+
+	while (fast != NULL and fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+			break;
+	}
+
+	// If we don't find any loop
+	if (slow != fast)
+		return NULL;
+
+	// If they both were equal indicating a cycle, then to find the first element hmlog ek pointer ko first pe le jaate and then run again at same speed.
+	// You know the mathematical proof too.
+	if (slow == fast) {
+		slow = head;
+		while (slow->next != fast->next) {
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+
+
+	return slow;
+}
