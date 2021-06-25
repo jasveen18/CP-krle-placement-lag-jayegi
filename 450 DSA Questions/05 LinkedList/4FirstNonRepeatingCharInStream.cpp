fdf5c4554774a3abd28e3ae4225bfbc3c34a98c7
Arpit <arpitfalcon1@gmail.com>
2021-06-25 22:31:10 +0530

    LL: first non-repeating char

--- 450 DSA Questions/05 LinkedList/4FirstNonRepeatingCharInStream.cpp
+++ 450 DSA Questions/05 LinkedList/4FirstNonRepeatingCharInStream.cpp
@@ -9,6 +9,28 @@
 // Output: "a#bb"
 
 string FirstNonRepeating(string A) {
+	queue<char> q;
+	string res = "";
 
+	int seen[26] = {0};
 
+	for (int i = 0; i < A.size(); i++) {
+		q.push(A[i]);
+		seen[A[i] - 'a']++;
+
+		while (!q.empty()) {
+			if (seen[q.front() - 'a'] > 1)
+				q.pop();
+			else
+				break;
+		}
+
+		if (q.empty()) {
+			res += '#';
+		} else {
+			res += q.front();
+		}
+	}
+
+	return res;
 }
