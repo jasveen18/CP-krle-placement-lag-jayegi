43cab59975b3cf21dc9e4209dc65d1a476f5c263
Arpit <arpitfalcon1@gmail.com>
2021-06-25 10:02:28 +0530

    LL: merge K sorted LL -- 2

--- 450 DSA Questions/05 LinkedList/5MergeKSortedLL.cpp
+++ 450 DSA Questions/05 LinkedList/5MergeKSortedLL.cpp
@@ -24,9 +24,6 @@ Node *merge2Lists(Node* left, Node* right) {
 }
 
 Node * mergeKLists(Node *arr[], int K) {
-    Node* startList = NULL;
-    Node* traversal = NULL;
-
     for (int i = 1; i < K; i++) {
         arr[0] = merge2Lists(arr[0], arr[i]);
     }
@@ -35,5 +32,63 @@ Node * mergeKLists(Node *arr[], int K) {
 }
 
 
-// Approach 1 - Merge 2 lists one by one
-// T - O(N*K*LogK) | S - O(1)
+// Approach 2 - Merge 2 lists using DnC.
+// T - O(N*K*LogK) | S - O(K) (recrusive stack)
+Node* mergeKListsDnC(Node* arr[], int start, int end) {
+    // Base Case
+    if (start == end)
+        return arr[start];
+    if (start > end)
+        return NULL;
+
+    // Recursive Case
+    int mid = (start + end) / 2;
+    Node* l1 = mergeKListsDnC(arr, start, mid);
+    Node* l2 = mergeKListsDnC(arr, mid + 1, end);
+
+    return merge2Lists(l1, l2);
+}
+
+Node * mergeKLists(Node *arr[], int K) {
+    if (K == 0)
+        return NULL;
+
+    return mergeKListsDnC(arr, 0, K - 1);
+}
+
+
+// Approach 3 - Merge 2 lists using Priority Queue
+// T - O(N*K*LogK) | S - O(K) (PQ ka space)
+
+// Min priority queue
+class MyComparator {
+public:
+    int operator()(Node *a, Node *b) {
+        return b->data < a->data;
+    }
+};
+
+
+Node * mergeKLists(Node *arr[], int K) {
+    priority_queue<Node*, vector<Node*>, MyComparator> pq;
+
+    for (int i = 0; i < K; i++) {
+        if (arr[i])
+            pq.push(arr[i]);
+    }
+
+    Node* res = new Node(-1);
+    Node* curr = res;
+
+    while (!pq.empty()) {
+        Node* temp = pq.top(); pq.pop();
+
+        if (temp->next)
+            pq.push(temp->next);
+
+        curr->next = temp;
+        curr = curr->next;
+    }
+
+    return res->next;
+}
