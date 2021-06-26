d8ad07ce8d9224260a9d15912f1f6793b554ab86
Arpit <arpitfalcon1@gmail.com>
2021-06-26 07:13:29 +0530

    trees: right view

---
+++ 450 DSA Questions/06 Binary Trees/3RightView.cpp
@@ -0,0 +1,44 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Return the right view of the tree
+
+vector<int> rightViewTree(Node *root) {
+    if (root == NULL)
+        return {};
+
+    queue<Node*> q;
+    vector<vector<int>> res;
+    vector<int> temp;
+
+    q.push(root);
+
+    while (!q.empty()) {
+        int sizeHere = q.size();
+        for (int i = 0; i < sizeHere; i++) {
+            Node* curr = q.front(); q.pop();
+
+            // Perform the desired operation
+            temp.push_back(curr->data);
+
+            // Push the child nodes.
+            if (curr->left) q.push(curr->left);
+            if (curr->right) q.push(curr->right);
+        }
+
+        // Push this whole level to the res vector now
+        res.push_back(temp);
+        temp.clear();
+    }
+
+    vector<int> ans;
+
+    for (int i = 0; i < res.size(); i++) {
+        ans.push_back(res[i][res[i].size() - 1]);
+    }
+
+    return ans;
+}
