de9453051d17c4fc3ff8ba0b79947a934a842b6d
Arpit <arpitfalcon1@gmail.com>
2021-06-26 05:31:20 +0530

    trees: level order, with new levels in each line

--- 450 DSA Questions/06 Binary Trees/4TraversalLevelOrder.cpp
+++ 450 DSA Questions/06 Binary Trees/4TraversalLevelOrder.cpp
@@ -32,3 +32,33 @@ vector<int> levelOrder(Node* node) {
 
 
 // Print the traversal in multiple lines as per their level
+vector<vector<int>> levelOrder(Node* node) {
+    if (node == NULL)
+        return {};
+
+    queue<Node*> q;
+    vector<vector<int>> res;
+    vector<int> temp;
+
+    q.push(node);
+
+    while (!q.empty()) {
+        int sizeHere = q.size();
+        for (int i = 0; i < sizeHere; i++) {
+            Node* curr = q.front(); q.pop();
+
+            // Perform the desired operation
+            temp.push_back(curr->data);
+
+            // Push the child nodes.
+            if (curr->left) q.push(curr->left);
+            if (curr->right) q.push(curr->right);
+        }
+
+        // Push this whole level to the res vector now
+        res.push_back(temp);
+        temp.clear();
+    }
+
+    return res;
+}
