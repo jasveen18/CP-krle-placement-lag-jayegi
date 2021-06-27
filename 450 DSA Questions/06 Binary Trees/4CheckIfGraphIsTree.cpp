18f6913801b79f386ba4ab2891bc45b3eb5244e9
Arpit <arpitfalcon1@gmail.com>
2021-06-27 07:17:00 +0530

    tree: is graph a tree?

---
+++ 450 DSA Questions/06 Binary Trees/4CheckIfGraphIsTree.cpp
@@ -0,0 +1,17 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Check if a graph is a tree.
+//
+
+/* For a graph to be tree -
+*  1. Graph should be connected.
+*  2. It should not contain any cycle.
+*
+*
+* Use any algorithm to do cycle detection and also check that
+* the every node is marked in visited array in one go.
+* /
