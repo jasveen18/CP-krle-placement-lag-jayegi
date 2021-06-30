02c6c92e7e90d299029de0657135e63023f1f659
Arpit <arpitfalcon1@gmail.com>
2021-06-30 09:23:23 +0530

    heap: sum of k1 and k2 smallest

---
+++ Heap Bootcamp/10-sumOfElementk1k2.cpp
@@ -0,0 +1,13 @@
+// ** Sum of elements of k1th smallest and k2th smallest ** //
+
+// Problem Statement - Return the sum of k1th smallest and k2th smallest number.
+
+// Identification -
+// 1. k dia hai.
+// 2. smallest hai.
+
+// Approach -
+// k smallest number ka function,
+
+// call twice for k1, and k2.
+// return sum.
