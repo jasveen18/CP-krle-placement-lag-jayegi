4aee9cb28dd07ac5ed4e8a06eed21c7b779c8c58
Arpit <arpitfalcon1@gmail.com>
2021-06-30 09:12:51 +0530

    heap: k closest point

---
+++ Heap Bootcamp/08-kClosestPointToOrigin.cpp
@@ -0,0 +1,32 @@
+// ** K Closest Point to Origin ** //
+
+// Problem Statement - Given array of points, find k closest points.
+
+// Identification -
+// 1. k dia hai.
+// 2. closest puch ra hai.
+
+
+int distanceFormula(pair<int, int> a) {
+	return a.first * a.first + a.second * a.second;
+}
+
+vector<pair<int, int>> kClosestPoints(vector<pair<int, int>> &points, int n, int k) {
+	priority_queue < pair<int, pair<int, int>> maxHeap;
+	// Key ke jagah sqrt(x^2 + y^2) ke jagah pe sqrt ni daalte hai, answer toh shii hi aayega
+	// since relative.
+
+	for (int i = 0; i < n; i++) {
+		maxHeap.push({distanceFormula(nums[i]), nums[i]});
+
+		if (maxHeap.size() > k) {
+			maxHeap.pop();
+		}
+	}
+
+	while (!maxHeap.empty()) {
+		// Print the answer
+	}
+
+	return;
+}
