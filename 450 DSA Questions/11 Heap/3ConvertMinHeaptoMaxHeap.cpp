8b47fdb97734cfdfbafce76c5c1ec4ecbae1e527
Arpit <arpitfalcon1@gmail.com>
2021-07-01 11:45:39 +0530

    heap: convert min to max

---
+++ 450 DSA Questions/11 Heap/3ConvertMinHeaptoMaxHeap.cpp
@@ -0,0 +1,10 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Convert a min heap to max heap.
+
+// Just start from first non-leaf node and do max heapify.
+// No logic, same as heap implementation.
