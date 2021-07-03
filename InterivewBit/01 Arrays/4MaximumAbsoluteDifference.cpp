92053fd7cc47b2ed99e86f4f243378d97137d594
Arpit <arpitfalcon1@gmail.com>
2021-07-03 15:06:41 +0530

    ib: 5 - editorial

--- InterivewBit/01 Arrays/4MaximumAbsoluteDifference.cpp
+++ InterivewBit/01 Arrays/4MaximumAbsoluteDifference.cpp
@@ -37,3 +37,23 @@ int Solution::maxArr(vector<int> &A) {
 
     return maxOfFour(max1 - min1, max2 - min2, max3 - min3, max4 - min4);
 }
+
+
+
+// Editorial -
+int Solution::maxArr(vector<int> &A) {
+    int ans = 0, n = A.size();
+
+    int max1 = INT_MIN, max2 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        max1 = max(max1, A[i] + i);
+        max2 = max(max2, A[i] - i);
+        min1 = min(min1, A[i] + i);
+        min2 = min(min2, A[i] - i);
+    }
+    ans = max(ans, max2 - min2);
+    ans = max(ans, max1 - min1);
+    return ans;
+}
