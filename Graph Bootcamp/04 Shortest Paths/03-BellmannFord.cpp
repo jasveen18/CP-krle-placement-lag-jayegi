d1326d24a53af10dbcc12bb287427df578b0eccb
Arpit <arpitfalcon1@gmail.com>
2021-07-03 18:18:14 +0530

    graph: bellman ford

---
+++ Graph Bootcamp/04 Shortest Paths/03-BellmannFord.cpp
@@ -0,0 +1,42 @@
+// *** BellMann Ford Shortest Path / Cycle Detection *** //
+
+// It is used when we have a negative weighted graph (negative edges).
+// The issue here is to minimize the distance, we will fall into an inf cycle.
+// Used to detect negative cycle / single source shortest path (can't use dijkstra as it'll end up in a inf loop)
+
+// Approach -
+// 1. Initialize all the distances as infinite
+// 2. Relax all the edges, V-1 times (Bottom up DP jaise)
+// 3. Detect Negative Weight Cycle to avoid negative infinite.
+
+// We will use a edge list to implement this.
+vector<int> bellmanFord(int V, int src, vector<vector<int>> edgeList) {
+	// 1. Create the distance vector
+	vector<int> dist(V + 1, INT_MAX);
+	dist[src] = 0; // Since source -> source, toh zero hoga.
+
+	// 2. Relax all edges (V-1) times. Why?
+	for (int i = 0; i < V - 1; i++) {
+		for (auto edge : edgeList) {
+			int u = edge[0], v = edge[1], wt = edge[2];
+
+			// Check for possible overflow and relax the edge
+			if (dist[u] != INT_MAX and dist[u] + wt < dist[v])
+				dist[v] = dist[u] + wt;
+		}
+	}
+
+	// 3. Check for negative weight cycle
+	for (auto edge : edgeList) {
+		int u = edge[0], v = edge[1], wt = edge[2];
+
+		// If after running our algorithm, the weights can still be updated.
+		// then cycle present hai.
+		if (dist[u] != INT_MAX and dist[u] + wt < dist[v]) {
+			// This means negative weight cycle present hai, and we can't find distances.
+			return { -1};
+		}
+	}
+
+	return dist;
+}
