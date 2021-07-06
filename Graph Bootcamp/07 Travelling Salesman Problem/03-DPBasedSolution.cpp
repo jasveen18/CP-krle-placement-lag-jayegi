46211a1482142d4bfba084f61a268e44cd81f3f9
Arpit <arpitfalcon1@gmail.com>
2021-07-06 08:32:23 +0530

    graph: travelling salesman dp

--- Graph Bootcamp/07 Travelling Salesman Problem/03-DPBasedSolution.cpp
+++ Graph Bootcamp/07 Travelling Salesman Problem/03-DPBasedSolution.cpp
@@ -1,16 +1,20 @@
 // ** Traverlling Sales Man - DP with Bitmasking ** //
-
+// O(N!) -> O(2^N * N)
 
 // Set of cities, is the visited cities and we are using the bits of the number to represent this.
 // num = 00000
 // num = 00001, means ki A is visited
 // We are using this in DP(next solution), to hash the value very easily.
-int tspSolve(vector<vector<int>> &dist, int setOfCities, int city, int n) {
+int tspSolve(vector<vector<int>> &dist, int setOfCities, int city, int n, vector<vector<int>> &dp) {
 	// Base Case -     2^n - 1, 111111.. aisa kuch mtlb sab jagah pahuch gye
 	if (setOfCities == ((1 << n) - 1)) {
 		return dist[city][0]; // Return the cost of current city to source
 	}
 
+
+	// Dp lookup
+	if (dp[setOfCities][city] != -1)
+		return dp[setOfCities][city];
 
 	// Recursive Case - Try all possible options -
 	int ans = INT_MAX;
@@ -20,16 +24,17 @@ int tspSolve(vector<vector<int>> &dist, int setOfCities, int city, int n) {
 		if ((setOfCities & (1 << choice)) == 0) {
 
 			int setThisCity = (setOfCities | (1 << choice)); // mark this city as true
-			int subProblem = dist[city][choice] + tspSolve(dist, setThisCity, choice, n);
+			int subProblem = dist[city][choice] + tspSolve(dist, setThisCity, choice, n, dp);
 			ans = min(ans, subProblem);
 
 		}
 	}
 
-	return ans;
+	return dp[setOfCities][city] = ans;
 }
 
 
 int travellingSalesMan(vector<vector<int>> &adjMatrix, int n) {
-	return tspSolve(adjMatrix, 1, n);
+	vector<vector<int>> dp(1 << n, vector<int> (n, -1));
+	return tspSolve(adjMatrix, 1, n, dp);
 }
