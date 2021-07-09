30af50e1c905f9a25db5a32b256e8e0b41a077ec
Arpit <arpitfalcon1@gmail.com>
2021-07-09 22:04:18 +0530

    ib: count and say

---
+++ InterivewBit/03 Strings/3CountAndSay.cpp
@@ -0,0 +1,36 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Count and say problem
+string countAndSay(int n) {
+    // Base Case
+    if (n == 1)
+        return "1";
+
+    // Recursive Case
+    string subProb = countAndSay(n - 1);
+    int count = 0;
+    char num = subProb[0];
+
+    // Result for this call
+    string res = "";
+
+    // Count and say
+    for (int i = 0; i < subProb.size(); i++) {
+        if (num == subProb[i]) {
+            count++;
+        } else {
+            res += to_string(count) + num;
+            num = subProb[i];
+            count = 1;
+        }
+    }
+
+    // Add the left over part
+    res += (to_string(count) + num);
+
+    return res;
+}
