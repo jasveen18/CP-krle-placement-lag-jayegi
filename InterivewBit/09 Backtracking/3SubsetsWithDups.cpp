36b066e675ffce9f38bb1e36bf16a7ea0e761576
Arpit <arpitfalcon1@gmail.com>
2021-07-10 18:50:06 +0530

    ib: subset with dups

---
+++ InterivewBit/09 Backtracking/3SubsetsWithDups.cpp
@@ -0,0 +1,45 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Find subsets with dups.
+void findTheSubsets(vector<int> &nums, int i, vector<int> &curr, vector<vector<int>> &res) {
+    // Base Case
+    if (i == nums.size()) {
+        res.push_back(curr);
+        return;
+    }
+
+    // Recursive Case -
+    // 1. Exclude this element
+    findTheSubsets(nums, i + 1, curr, res);
+
+    // Include this
+    curr.push_back(nums[i]);
+    findTheSubsets(nums, i + 1, curr, res);
+
+    // Backtrack
+    curr.pop_back();
+    return;
+}
+
+
+vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
+    vector<vector<int>> res;
+    vector<int> curr;
+
+    findTheSubsets(A, 0, curr, res);
+
+    for (int i = 0; i < res.size(); i++)
+        sort(res[i].begin(), res[i].end());
+
+    sort(res.begin(), res.end());
+
+    // Remove duplicates subsets
+    auto ip = unique(res.begin(), res.end());
+    res.erase(ip, res.end());
+
+    return res;
+}
