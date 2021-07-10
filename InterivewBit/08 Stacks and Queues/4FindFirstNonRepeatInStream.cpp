096e2bec470d9520292e887acb455ebb27f389da
Arpit <arpitfalcon1@gmail.com>
2021-07-10 07:51:48 +0530

    ib: find first non-repeat

---
+++ InterivewBit/08 Stacks and Queues/4FindFirstNonRepeatInStream.cpp
@@ -0,0 +1,34 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Find first non repeat in stream.
+string Solution::solve(string A) {
+	int n = A.size();
+	unordered_map<char, int> count;
+	queue<char> first;
+
+	string res = "";
+
+	for (int i = 0; i < n; i++) {
+		// Mark as visited
+		count[A[i]]++;
+
+		// If first occurance then push into queue
+		if (count[A[i]] == 1) {
+			first.push(A[i]);
+		} else {  // If first ni hai toh, find the next element with 1 occurance
+			while (first.empty() == false and count[first.front()] >= 2) first.pop();
+		}
+
+		// Insert char as per available character
+		if (first.empty())
+			res += '#';
+		else
+			res += first.front();
+	}
+
+	return res;
+}
