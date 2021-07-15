4201d6e9505376d9130fd3eacdb48ecfd7978718
Arpit <arpitfalcon1@gmail.com>
2021-07-15 06:11:48 +0530

    wc244: q1, q2, q3

---
+++ LC Contests/WC244.cpp
@@ -0,0 +1,133 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// 1.1886. Determine Whether Matrix Can Be Obtained By Rotation
+void rotateMatrix(vector<vector<int>> &mat) {
+	// First take transpose
+	for (int i = 0; i < mat.size(); i++) {
+		for (int j = 0; j < mat.size(); j++) {
+			if (i < j) {
+				swap(mat[i][j], mat[j][i]);
+			}
+		}
+	}
+
+	// Rotate the rows
+	for (int i = 0; i < mat.size(); i++)
+		reverse(mat[i].begin(), mat[i].end());
+
+	return;
+}
+
+
+bool isSame(vector<vector<int>>& mat, vector<vector<int>>& target) {
+	int n = mat.size();
+	int m = target.size();
+
+	if (n != m)
+		return false;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (mat[i][j] != target[i][j])
+				return false;
+		}
+	}
+
+	return true;
+}
+
+bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+	bool ans = isSame(mat, target);
+
+	if (ans)
+		return ans;
+
+	for (int i = 0; i < 3; i++) {
+		rotateMatrix(mat);
+		if (isSame(mat, target))
+			return true;
+	}
+
+	return false;
+}
+
+
+// 2. 1887. Reduction Operations to Make the Array Elements Equal
+int reductionOperations(vector<int>& nums) {
+	int addition = 0;
+	int ans = 0;
+	int n = nums.size();
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 1; i < n; i++) {
+		if (nums[i] != nums[i - 1]) {
+			addition++;
+		}
+
+		ans += addition;
+	}
+
+	return ans;
+}
+
+
+// 3. 1888. Minimum Number of Flips to Make the Binary String Alternating
+int minFlips(string s) {
+	int n = s.size();
+
+	// Cyclic Rotation hai toh end pe same string add kro
+	// Will slide a window of size 'n'.
+	s = s + s;
+
+	// Now find the min number of operations
+	int zeroAtEven = 0;
+	int oneAtEven = 0;
+	int ans = INT_MAX;
+
+	int i = 0, j = 0;
+	while (j < s.size()) {
+		// Do the calculations
+		// Zero at even place
+		if ((j & 1) and s[j] != '1')
+			zeroAtEven++;
+		if (!(j & 1) and s[j] != '0')
+			zeroAtEven++;
+
+		// One at even place
+		if ((j & 1) and s[j] != '0')
+			oneAtEven++;
+		if (!(j & 1) and s[j] != '1')
+			oneAtEven++;
+
+		if (j - i + 1 < n) { // Window hit ni hua hai
+			j++;
+		} else if (j - i + 1 == n) { // Window hit ho gya hai
+			ans = min(ans, zeroAtEven);
+			ans = min(ans, oneAtEven);
+
+			// Now slide the window
+			// Zero at even place
+			if ((i & 1) and s[i] != '1')
+				zeroAtEven--;
+			if (!(i & 1) and s[i] != '0')
+				zeroAtEven--;
+
+			// One at even place
+			if ((i & 1) and s[i] != '0')
+				oneAtEven--;
+			if (!(i & 1) and s[i] != '1')
+				oneAtEven--;
+
+
+			// Increment i and j
+			i++; j++;
+		}
+	}
+
+	return ans;
+}
