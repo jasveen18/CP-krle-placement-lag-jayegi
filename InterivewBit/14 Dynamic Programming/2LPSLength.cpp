390019ff7a57cdd4cc235acbef31ff9314645b44
Arpit <arpitfalcon1@gmail.com>
2021-07-16 20:17:44 +0530

    ib: LPS

---
+++ InterivewBit/14 Dynamic Programming/2LPSLength.cpp
@@ -0,0 +1,36 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - LPS Length
+int LCS(string A, string B) {
+    int n = A.size();
+    int m = B.size();
+
+    // Initialize DP
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // DP will have zero at first row and column
+
+    // Build up the dp
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
+int Solution::solve(string A) {
+    string B = A;
+    reverse(B.begin(), B.end());
+
+    return LCS(A, B);
+}
+
