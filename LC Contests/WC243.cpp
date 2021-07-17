d6bacd4e68aed0a603928548d38fc338515dfc7f
Arpit <arpitfalcon1@gmail.com>
2021-07-17 16:23:06 +0530

    lc243: q3

--- LC Contests/WC243.cpp
+++ LC Contests/WC243.cpp
@@ -58,3 +58,43 @@ string maxValue(string n, int x) {
 
 	return res;
 }
+
+
+// 3. 1882. Process Tasks Using Servers
+
+// Here, we have two min heaps, ordered by [time, weight, index]: available and busy. First, we put all servers into the available heap
+// with time equals to zero.
+
+// As we process our tasks:
+
+// Move servers that have finished (time <= t) from busy to avail.
+// Pick the top server from the avail heap and move it to the busy heap with time = t + tasks[t].
+// This is where we record the index of the server for that task.
+// What happens if no servers are available? In this case, the server that is closest to finishing its task will pick up the new task. So:
+
+// We move the top server from busy to avail heap.
+// Then we move it back to the busy heap, but this time we extend the completion time for the duration of the new task time + task[t].
+// In other words, the time is t + task[t] if we have an available server vs. time + task[t].
+
+vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
+	// [time, weight, index]
+	priority_queue<array<long, 3>, vector<array<long, 3>>, greater<array<long, 3>>> available, busy;
+	vector<int> res;
+
+	for (int i = 0; i < servers.size(); i++) {
+		available.push({0, servers[i], i});
+	}
+
+	for (long t = 0; t < tasks.size(); t++) {
+		while (!busy.empty() and (busy.top()[0] <= t or available.empty())) {
+			auto [time, weight, idx] = busy.top(); busy.pop();
+			available.push({time <= t ? 0 : time, weight, idx});
+		}
+
+		auto [time, weight, idx] = available.top(); available.pop();
+		busy.push({max(time, t) + tasks[t], weight, idx});
+		res.push_back(idx);
+	}
+
+	return res;
+}
