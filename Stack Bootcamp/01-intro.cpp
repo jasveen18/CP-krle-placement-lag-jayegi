cc4cc41832b65bf3c64a6e2bef7460b26ff3f5ff
Arpit <arpitfalcon1@gmail.com>
2021-07-18 08:34:41 +0530

    stacks: intro

---
+++ Stack Bootcamp/01-intro.cpp
@@ -0,0 +1,37 @@
+// ** Stack Intro ** //
+
+// Mostly saare questions have a parent questions, uske hi variations aate rehte hai.
+
+// Stacks ke variations ->
+
+// 01. Nearest Greater to left
+// 02. Nearest Greater to right
+// 03. Nearest Smaller to left
+// 04. Nearest Smaller to right
+
+// -> Upar ke chaar use krke 5, 6 solve
+// 05. Stock Span Problem
+// 06. Max area in histogram
+
+// -> 5, 6 use krke 7
+// 07. Max Area of rectangle in Binary Matrix
+
+// -> These are some good questions
+// 08. Rain water problem
+// 09. Implementing a min stack (with and without space)
+// 10. Implement Stack using heap
+// 11. The celebrity problem
+// 12. Longest Valid Parentheses
+// 13. Iterative Tower of Hanoi
+
+
+// Ek normal nested loop ->
+// for(int i=0; i<n; i++)
+// 	for(int j=0; j<n; j++)  // Independent
+
+// Dependant loop
+// for(i to n)
+// 	for(j = 0; j<i; j++)
+// Here j is depending on i. aisa hi j 0 to i, i to 0, i to n, n to i hai
+// If j loop is dependant on 'i', there is an improvised method using stack.
+
