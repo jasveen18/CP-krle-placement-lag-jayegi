18f5dce92bd378707e67f03def7e5b62b89db200
Arpit <arpitfalcon1@gmail.com>
2021-07-19 08:22:38 +0530

    queue: level order anagram

---
+++ 450 DSA Questions/10 Stacks and Queues/3CheckIfLevelOfTreeisAnagram.cpp
@@ -0,0 +1,12 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Check if all the levels of two trees are anagrams or not.
+
+// Simple solutions ->
+// Store the level order for both tree and then sort and compare.
+// Alternative approach -> store in a map of first tree and then second tree ka (--) krte jao.
+// Agar koi element bach jaata hai toh return false. or continue.
