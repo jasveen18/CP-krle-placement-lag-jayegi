1e96ae590b663b2010f08bca4b82c15efb730b93
Arpit <arpitfalcon1@gmail.com>
2021-07-21 05:10:04 +0530

    wc239: q2 q4

--- LC Contests/WC239.cpp
+++ LC Contests/WC239.cpp
@@ -24,10 +24,22 @@ int getMinDistance(vector<int>& nums, int target, int start) {
 	return -1;
 }
 
+// 2. 1849. Splitting a String Into Descending Consecutive Values
+bool splitString(string s, int i = 0, long prev = 0) {
+	long num = 0;
+	for (int j = i; num < 1e10 and j < s.size() - (i == 0 ? 1 : 0); j++) {
+		num = num * 10 + (s[j] - '0');
+
+		// Try with this combinations
+		if ((i == 0 or prev - 1 == num) and splitString(s, j + 1, num))
+			return true;
+	}
+
+	return i == s.size();
+}
 
 
-
-// 3.
+// 3. 1850. Minimum Adjacent Swaps to Reach the Kth Smallest Number
 void nextPermutation(string &s) {
 	int n = s.size();
 	int i = n - 2;
@@ -75,4 +87,50 @@ int getMinSwaps(string num, int k) {
 	}
 
 	return res;
+}
+
+
+// 4. 1851. Minimum Interval to Include Each Query
+
+// Sort queries and intervals.
+// Iterate queries from small to big,
+// and find out all open intervals [l, r],
+// and we add them to a priority queue.
+// Also, we need to remove all closed interval from the queue.
+
+// In the priority, we use
+// [interval size, interval end] = [r-l+1, r] as the key.
+
+// The head of the queue is the smallest interval we want to return for each query.
+vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+	// Interval size, interval end -> r - l + 1, r
+	priority_queue<vector<int>> pq;
+
+	// To store the answer, and later re-order them into the order of queries
+	unordered_map<int, int> mp;
+
+	// Sort the intervals and queries
+	vector<int> Q = queries, ans;
+	sort(intervals.begin(), intervals.end());
+	sort(Q.begin(), Q.end());
+
+	int i = 0, n = intervals.size();
+	for (auto q : Q) {
+		while (i < n and intervals[i][0] <= q) {
+			int l = intervals[i][0], r = intervals[i][1]; i++;
+
+			// Notice ki yaha pe l - r - 1 daal re hai instead of r-l+1, to change order of PQ.
+			pq.push({l - r - 1, r});
+		}
+
+		while (pq.size() and pq.top()[1] < q)
+			pq.pop();
+
+		mp[q] = pq.size() ? (-pq.top()[0]) : -1;
+	}
+
+	for (int q : queries)
+		ans.push_back(mp[q]);
+
+	return ans;
 }
