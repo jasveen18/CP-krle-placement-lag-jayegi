4b6266e43ec94fa5186ce3b184b5f9aa0b730424
Arpit <arpitfalcon1@gmail.com>
2021-07-23 05:24:53 +0530

    ib: paint house

---
+++ InterivewBit/14 Dynamic Programming/2PaintHouse.cpp
@@ -0,0 +1,23 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+******************************************/
+
+// Problem Statement - Paint the house with no adj color and lowest cost.
+int Solution::solve(vector<vector<int> > &A) {
+    int n = A.size();
+
+    vector<vector<int>> dp(A.size(), vector<int> (3, 0));
+    dp[0][0] = A[0][0];
+    dp[0][1] = A[0][1];
+    dp[0][2] = A[0][2];
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = A[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+        dp[i][1] = A[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+        dp[i][2] = A[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
+    }
+
+    return min(dp[n - 1][0], min(dp[n - 1][1], dp[n - 1][2]));
+}
