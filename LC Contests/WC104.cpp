dab03307710b31ab06bd8dafb5d610e394025218
Arpit <arpitfalcon1@gmail.com>
2021-08-01 05:01:13 +0530

    wc105: 3

--- LC Contests/WC104.cpp
+++ LC Contests/WC104.cpp
@@ -42,3 +42,48 @@ int partitionDisjoint(vector<int>& nums) {
 
 	return n;
 }
+
+// 3. Word Subsets
+vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+	// First process words2 -> max freq of each letter in each word. O(26 * N)
+	unordered_map<int, int> freq;
+	vector<int> maxrequired(26, 0);
+	for (auto word : words2) {
+		freq.clear();
+
+		// Count
+		for (auto ch : word) {
+			freq[ch - 'a']++;
+		}
+
+		// Take max
+		for (auto el : freq) {
+			maxrequired[el.first] = max(maxrequired[el.first], el.second);
+		}
+	}
+
+	// Now let's see in words1 for each word if it can satisfy max, it can satisfy all
+	vector<string> res;
+	freq.clear();
+
+	for (auto word : words1) {
+		bool yes = true;
+		freq.clear();
+
+		for (auto ch : word) {
+			freq[ch - 'a']++;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (maxrequired[i] > freq[i]) {
+				yes = false;
+				break;
+			}
+		}
+
+		if (yes)
+			res.push_back(word);
+	}
+
+	return res;
+}
