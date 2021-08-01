9cc80caf00496ff479dc833d14fdf7058925e3a2
Arpit <arpitfalcon1@gmail.com>
2021-08-01 10:07:26 +0530

    wc106: 1

---
+++ LC Contests/WC106.cpp
@@ -0,0 +1,32 @@
+//1 . 922. Sort Array By Parity II
+class Solution {
+public:
+	vector<int> sortArrayByParityII(vector<int>& nums) {
+		int even = 0;
+		int odd = 0;
+		int n = nums.size();
+
+		while (even < n and nums[even] % 2 == 0)
+			even += 2;
+
+		if (even >= n)
+			return nums;
+
+		odd = 1;
+		while (odd < n and nums[odd] % 2 == 1)
+			odd += 2;
+
+
+		while (even < n and odd < n) {
+			swap(nums[even], nums[odd]);
+
+			while (even < n and nums[even] % 2 == 0)
+				even += 2;
+
+			while (odd < n and nums[odd] % 2 == 1)
+				odd += 2;
+		}
+
+		return nums;
+	}
+};
