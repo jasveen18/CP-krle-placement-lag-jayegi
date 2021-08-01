8acaaf340d9192a060f65b61013cd7cc23c6192f
Arpit <arpitfalcon1@gmail.com>
2021-08-01 06:55:39 +0530

    wc105; 3

--- LC Contests/WC105.cpp
+++ LC Contests/WC105.cpp
@@ -25,3 +25,96 @@ string reverseOnlyLetters(string s) {
 
 	return res;
 }
+
+
+
+// 3. Complete Binary Tree Inserter
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class CBTInserter {
+private:
+	int insert(TreeNode* root, int v, bool &ins, int maxh, int h) {
+		if (ins)
+			return -1;
+		// Check for both nodes
+		if (root->left == NULL and root->right == NULL and h == maxh)
+			return -1;
+
+		if (root->left == NULL) {
+			root->left = new TreeNode(v);
+			ins = true;
+			return root->val;
+		}
+
+		if (root->right == NULL) {
+			root->right = new TreeNode(v);
+			ins = true;
+			return root->val;
+		}
+
+		int left = insert(root->left, v, ins, maxh, h + 1);
+		int right = insert(root->right, v, ins, maxh, h + 1);
+
+		if (left != -1)
+			return left;
+		if (right != -1)
+			return right;
+
+		return -1;
+	}
+
+public:
+	TreeNode* root;
+	CBTInserter(TreeNode* root) {
+		this->root = root;
+	}
+
+	int insert(int v) {
+		// 2 case banate hai,
+		// 1. If it is complete fully (filled)
+		// 2. If koi ek level pe missing hai toh vaha daal dena. (height bhi maintain kr lo)
+		// Try on second case first, if that fails then left most pe insert kr denge
+
+		int maxh = 0;
+		TreeNode* temp = root;
+		while (temp->left != NULL) {
+			temp = temp->left;
+			maxh++;
+		}
+
+		bool inserted = false;
+		TreeNode* curr = root;
+		int res = insert(curr, v, inserted, maxh, 0);
+
+		if (inserted)
+			return res;
+
+		// Now first case since it is a completely filled tree
+		curr = root;
+		while (curr->left != NULL)
+			curr = curr->left;
+
+		curr->left = new TreeNode(v);
+		return curr->val;
+	}
+
+	TreeNode* get_root() {
+		return this->root;
+	}
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter* obj = new CBTInserter(root);
+ * int param_1 = obj->insert(v);
+ * TreeNode* param_2 = obj->get_root();
+ */
