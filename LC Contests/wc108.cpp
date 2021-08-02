15df7984725ad460962b441d8ecfe8594a89fb55
Arpit <arpitfalcon1@gmail.com>
2021-08-02 07:11:42 +0530

    wc108: 1

--- LC Contests/wc108.cpp
+++ LC Contests/wc108.cpp
@@ -33,3 +33,31 @@ int numUniqueEmails(vector<string>& emails) {
 
     return res;
 }
+
+
+// 3. Min falling path sum
+int minFallingPathSum(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    if (n == 1)
+        return matrix[0][0];
+
+    vector<vector<int>> dp(n, vector<int> (n, 0));
+    for (int i = 0; i < n; i++)
+        dp[0][i] = matrix[0][i];
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n - 1; j++) {
+            dp[i][j] = matrix[i][j] + min(min(dp[i - 1][j - 1], dp[i - 1][j + 1]), dp[i - 1][j]);
+        }
+
+        // Boundaries
+        dp[i][0] = matrix[i][0] + min(dp[i - 1][1], dp[i - 1][0]);
+        dp[i][n - 1] = matrix[i][n - 1] + min(dp[i - 1][n - 2], dp[i - 1][n - 1]);
+    }
+
+    int res = INT_MAX;
+    for (int i = 0; i < n; i++)
+        res = min(res, dp[n - 1][i]);
+
+    return res;
+}
