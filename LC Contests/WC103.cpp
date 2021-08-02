393402da8c91a4efbfa8846799fd7edaaf67f2f6
Arpit <arpitfalcon1@gmail.com>
2021-08-02 08:58:46 +0530

    wc103: 1

--- LC Contests/WC103.cpp
+++ LC Contests/WC103.cpp
@@ -1,4 +1,11 @@
 // 1. Smallest Range I
+//     Intuition:
+// If min(A) + K < max(A) - K, then return max(A) - min(A) - 2 * K
+// If min(A) + K >= max(A) - K, then return 0
+int smallestRangeI(vector<int>& A, int K) {
+	return max(0, *max_element(begin(A), end(A)) - K - (*min_element(begin(A), end(A)) + K));
+}
+
 
 // 2. Snake and Ladders
 
