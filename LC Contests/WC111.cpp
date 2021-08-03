e0d09ce220f34b2c80a79bafaa151a9bc5c54623
Arpit <arpitfalcon1@gmail.com>
2021-08-03 08:28:24 +0530

    wc111: 2

---
+++ LC Contests/WC111.cpp
@@ -0,0 +1,18 @@
+
+
+// 2. 944. Delete Columns to Make Sorted
+int minDeletionSize(vector<string>& strs) {
+	int res = 0;
+
+	for (int i = 0; i < strs[0].size(); i++) {
+
+		for (int j = 0; j < strs.size() - 1; j++) {
+			if (strs[j][i] > strs[j + 1][i]) {
+				res++;
+				break;
+			}
+		}
+	}
+
+	return res;
+}
