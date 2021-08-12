20555b26c02c8720541abe07f3b6b7bd8d424116
Arpit <arpitfalcon1@gmail.com>
2021-08-12 07:35:01 +0530

    wc117: 2

--- LC Contests/WC117.cpp
+++ LC Contests/WC117.cpp
@@ -17,3 +17,45 @@ bool isUnivalTree(TreeNode* root, TreeNode* parent = NULL) {
 
 	return left and right;
 }
+
+
+// 2. 967. Numbers With Same Consecutive Differences
+class Solution {
+private:
+	void findAll(int n, int k, vector<string> &res, string curr) {
+		// Base
+		if (n == 0) {
+			res.push_back(curr);
+			return;
+		}
+
+		// Recursive
+		char digUp = curr[curr.size() - 1] + k;
+		if (digUp <= '9')
+			findAll(n - 1, k, res, curr + digUp);
+
+		char digDown = curr[curr.size() - 1] - k;
+		if (digDown >= '0' and digUp != digDown) // Extra check to remove duplicates when k = 0.
+			findAll(n - 1, k, res, curr + digDown);
+
+		return;
+	}
+
+public:
+	vector<int> numsSameConsecDiff(int n, int k) {
+		vector<string> res;
+		for (int i = 1; i <= 9; i++) {
+			string curr = "";
+			curr += (i + '0');
+			// cout<<curr<<endl;
+			findAll(n - 1, k, res, curr);
+		}
+
+		vector<int> finalRes;
+		for (auto num : res) {
+			finalRes.push_back(stoi(num));
+		}
+
+		return finalRes;
+	}
+};
