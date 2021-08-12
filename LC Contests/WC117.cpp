7d74e7dacd3fd0345341609011a4362280ecb501
Arpit <arpitfalcon1@gmail.com>
2021-08-12 08:00:09 +0530

    wc117: 3

--- LC Contests/WC117.cpp
+++ LC Contests/WC117.cpp
@@ -59,3 +59,58 @@ public:
 		return finalRes;
 	}
 };
+
+// 3. 966. Vowel Spellchecker
+class Solution {
+	string tolow(string s) {
+		for (auto &c : s) {
+			c = tolower(c);
+		}
+
+		return s;
+	}
+
+	string tonovowel(string s) {
+		s = tolow(s);
+
+		for (auto &c : s) {
+			if (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u')
+				c = '#';
+		}
+
+		return s;
+	}
+
+public:
+	vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+		unordered_set<string> words(wordlist.begin(), wordlist.end());
+		unordered_map<string, string> capital, novowel;
+
+		for (auto word : wordlist) {
+			string cap = tolow(word);
+			string dev = tonovowel(word);
+
+			capital.insert({cap, word});
+			novowel.insert({dev, word});
+		}
+
+		for (int i = 0; i < queries.size(); i++) {
+			if (words.find(queries[i]) != words.end())
+				continue;
+
+			// Now we need to modify and check
+			string lower = tolow(queries[i]);
+			string novow = tonovowel(queries[i]);
+
+			if (capital.find(lower) != capital.end()) {
+				queries[i] = capital[lower];
+			} else if (novowel.find(novow) != novowel.end()) {
+				queries[i] = novowel[novow];
+			} else {
+				queries[i] = "";
+			}
+		}
+
+		return queries;
+	}
+};
