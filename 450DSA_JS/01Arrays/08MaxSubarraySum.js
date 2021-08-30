4d5caafd0cdc1445a3099afa0c4296a28c76fee8
Arpit <arpitfalcon1@gmail.com>
2021-08-30 12:32:21 +0530

    add: max sum subarray

---
+++ 450DSA_JS/01Arrays/08MaxSubarraySum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n length
+ * @returns {number} max sum
+ */
+
+const maxSumSubarray = function (nums, n) {
+    let maxSumTillNow = nums[0];
+    let maxSumGlobal = nums[0];
+
+    for (let i = 1; i < n; i++) {
+        maxSumTillNow = Math.max(maxSumTillNow + nums[i], nums[i]);
+        maxSumGlobal = Math.max(maxSumGlobal, maxSumTillNow);
+    }
+
+    return maxSumGlobal;
+};
