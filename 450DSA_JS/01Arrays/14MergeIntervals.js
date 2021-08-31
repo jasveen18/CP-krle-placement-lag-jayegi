9ac15fa8a775df9240ee351136a54b1a126e9fec
Arpit <arpitfalcon1@gmail.com>
2021-08-31 18:46:35 +0530

    add: merge intervals

--- 450DSA_JS/01Arrays/14MergeIntervals.js
+++ 450DSA_JS/01Arrays/14MergeIntervals.js
@@ -2,7 +2,7 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-let merge = function (intervals) {
+let mergeIntervals = function (intervals) {
     intervals.sort((a, b) => {
         if (a[0] === b[0]) return a[1] - b[1];
         return a[0] - b[0];
